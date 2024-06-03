52dc37821a200fe0ee91c6d1d1978875e406947a
Charlie Gottlieb <charliegottlieb@gmail.com>
2024-06-03 12:52:33 -0400

    Initial Commit

---
+++ HistorTree/HistorTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistorTreeApp.swift
+//  HistorTree
+//
+//  Created by Charlie Gottlieb on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HistorTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
